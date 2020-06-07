8658ff5b3ba74374ee1988d36c47dbcb5fbe3da6
msisic96 <melika.sisic@hotmail.com>
2020-06-07 10:03:35 +0200

    Adding search functionality, prefix, suffix and double options

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var firstWord ="";
+var beginWordTreeWord = "";
+var currentType = '';
 
 function draw() {
     google.charts.load('current', {packages:['wordtree']});
@@ -7,6 +10,10 @@ function draw() {
       function drawChart() {
         var substrings = results[0].split('.')
         var finalArray = [['Phrases']];
+        if(firstWord === "") {
+            firstWord = substrings[0].split(" ")[0];
+        }
+        
         substrings.forEach(element => {
             finalArray.push([element + '.']);   
         });
@@ -17,13 +24,15 @@ function draw() {
         var options = {
           wordtree: {
             format: 'implicit',
-            word: 'cats',
-            fontName: 'Times-Roman'
+            type: currentType,
+            word: firstWord
           }
         };
 
         var chart = new google.visualization.WordTree(document.getElementById('wordtree_basic'));
         chart.draw(data, options);
+
+        this.dosth();
       }
 
 /*file inputs*/
@@ -62,3 +71,45 @@ function showResult() {
         console.log(results[i]);
     }
 }
+
+function dosth() {
+    var words = $( "#textoverview" ).first().text().split( /\s+/ );
+    var text = words.join( "</span> <span>" );
+    $( "#textoverview" ).first().html( "<span>" + text + "</span>" );
+    $( "span" ).on( "click", function() {
+    firstWord = this.textContent.replace(/\./g, '');
+    draw();
+    });
+}
+
+function searchWord() {
+    var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('search');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("textoverview");
+  span  = ul.getElementsByTagName('span');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < span.length; i++) {
+   
+    txtValue = span[i].textContent || span[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      span[i].style.display = "";
+    } else {
+      span[i].style.display = "none";
+    }
+  }
+}
+
+$('input[type=radio][name=treetype]').change(function() {
+    if (this.value === "prefix") {
+        currentType = '';
+    }
+    else if (this.value === "suffix") {
+        currentType = 'suffix';
+    }
+    else if (this.value === "double") {
+        currentType = 'double';
+    }
+    drawChart();
+});
