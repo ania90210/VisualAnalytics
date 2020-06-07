f603b1badc97aca3ad186d92483661e0329d793d
tarikbuza <buzatarik06@gmail.com>
2020-06-07 22:55:39 +0200

    Added colors on selected areas (should be expanded to work after tree click, but works for text area interaction), modified search box to have a button

--- index.js
+++ index.js
@@ -7,33 +7,35 @@ function draw() {
     google.charts.setOnLoadCallback(drawChart);
 }
 
-      function drawChart() {
-        var substrings = results[0].split('.')
-        var finalArray = [['Phrases']];
-        if(firstWord === "") {
-            firstWord = substrings[0].split(" ")[0];
-        }
-        
-        substrings.forEach(element => {
-            finalArray.push([element + '.']);   
-        });
-        var data = google.visualization.arrayToDataTable(finalArray
-         
-        );
+function drawChart() {
+    var substrings = results[0].split('.')
+    var finalArray = [['Phrases']];
+    if(firstWord === "") {
+        firstWord = substrings[0].split(" ")[0];
+    }
 
-        var options = {
-          wordtree: {
+    finalArray = finalArray.filter(function(e){
+        return e;
+    }); 
+
+    substrings.forEach(element => {
+        finalArray.push([element + '.']);   
+    });
+    var data = google.visualization.arrayToDataTable(finalArray);
+
+    var options = {
+        wordtree: {
             format: 'implicit',
             type: currentType,
             word: firstWord
-          }
-        };
+        }
+    };
 
-        var chart = new google.visualization.WordTree(document.getElementById('wordtree_basic'));
-        chart.draw(data, options);
+    var chart = new google.visualization.WordTree(document.getElementById('wordtree_basic'));
+    chart.draw(data, options);
 
-        this.dosth();
-      }
+    this.dosth();
+}
 
 /*file inputs*/
 var inputs;
@@ -66,6 +68,7 @@ function handleFile() {
     }
     this.draw();
 }
+
 function showResult() {
     for (var i = 0; i < inputs.length ; i++) {
         console.log(results[i]);
@@ -73,32 +76,55 @@ function showResult() {
 }
 
 function dosth() {
-    var words = $( "#textoverview" ).first().text().split( /\s+/ );
-    var text = words.join( "</span> <span>" );
-    $( "#textoverview" ).first().html( "<span>" + text + "</span>" );
-    $( "span" ).on( "click", function() {
-    firstWord = this.textContent.replace(/\./g, '');
-    draw();
+    var words = $("#textoverview").first().text().split(/(?=[.\s]|\b)/);
+    words.forEach(function(value, index, collection) {
+        var isPunctuation = !!value.match(/^[.,:!?]/)
+        var isSpace = !!value.match(/\s+/);
+        var isDot = !!value.match(/^[.]/)
+        if(!isPunctuation && !isSpace)
+            collection[index] = "<span class='word'>" + value + "</span>";
+        if(isPunctuation)
+            collection[index] = "<span class='punctuation'>" + value + "</span>";
+        if(isDot)
+            collection[index] = "<span class='dot'>" + value + "</span>";
+        if(isSpace)
+            collection[index] = "<span class='space'>" + value + "</span>";
+        
     });
+    var text = words.join("");
+    $("#textoverview").first().html(text);
+    $(".word").on( "click", function() {
+        firstWord = this.textContent.replace(/\./g, '');
+        draw();
+    });
+    
+    $('span').removeClass('yellow');
+    $('span').removeClass('red');
+    var wordSpans = $('.word:contains("'  + firstWord + '")').closest('span');
+    wordSpans.addClass('red');
+    var length  = wordSpans.length;
+    for (var i=0; i<length; i++) {
+        $(wordSpans[i]).nextUntil('span.dot').addClass('yellow');        
+        $(wordSpans[i]).nextUntil('span.dot').next("span").addClass('yellow');   
+    }
 }
 
 function searchWord() {
     var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('search');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("textoverview");
-  span  = ul.getElementsByTagName('span');
+    input = document.getElementById('search');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("textoverview");
+    span  = ul.getElementsByTagName('span');
 
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < span.length; i++) {
-   
-    txtValue = span[i].textContent || span[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      span[i].style.display = "";
-    } else {
-      span[i].style.display = "none";
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < span.length; i++) {
+        txtValue = span[i].textContent || span[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        span[i].style.display = "";
+        } else {
+        span[i].style.display = "none";
+        }
     }
-  }
 }
 
 $('input[type=radio][name=treetype]').change(function() {
